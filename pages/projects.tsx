1d1979c336c33a2c79824b914e434e7a8235e2b4
umerkang66 <umerkang66@gmail.com>
2022-07-26 17:57:42 +0500

    add project page title

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import { FC, Fragment, ReactElement } from 'react';
 import AllProjects from '../components/all-projects/all-projects';
 
 const Projects: FC = (): ReactElement => {
   return (
     <Fragment>
+      <Head>
+        <title>All Projects | Hammad</title>
+      </Head>
       <AllProjects />
     </Fragment>
   );
