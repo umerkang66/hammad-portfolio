620f1ca64edf33b717b4796a7f0f7b541cfbdb5e
umerkang66 <umerkang66@gmail.com>
2022-07-29 11:04:55 +0500

    removed github actions

--- pages/trainings.tsx
+++ pages/trainings.tsx
@@ -1,10 +1,14 @@
 import { Fragment } from 'react';
 import { NextPage } from 'next';
+import Head from 'next/head';
 import TrainingsComponent from '../components/trainings/trainings';
 
 const Trainings: NextPage = () => {
   return (
     <Fragment>
+      <Head>
+        <title>Trainings | Hammad</title>
+      </Head>
       <TrainingsComponent />
     </Fragment>
   );
