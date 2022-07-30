ee6b016f9667bb739c9bce0339def2f778fd5943
umerkang66 <umerkang66@gmail.com>
2022-07-30 13:54:13 +0500

    change excerptDescription

--- data/all-trainings.ts
+++ data/all-trainings.ts
@@ -4,25 +4,25 @@ export const allTrainings: Training[] = [
   {
     name: 'Training 1',
     excerptDesc:
-      'This is the first amazon training.-----This is the first amazon training.-----This is the first amazon training.-----This is the first amazon training.',
+      'This first amazon training.-----This first amazon training.-----This first amazon training.-----This first amazon training.',
     description: '',
   },
   {
     name: 'Training 2',
     excerptDesc:
-      'This is the second amazon training.-----This is the second amazon training.-----This is the second amazon training.-----This is the second amazon training.',
+      'This second amazon training.-----This second amazon training.-----This second amazon training.-----This second amazon training.',
     description: '',
   },
   {
     name: 'Training 3',
     excerptDesc:
-      'This is the third amazon training.-----This is the third amazon training.-----This is the third amazon training.-----This is the third amazon training.',
+      'This third amazon training.-----This third amazon training.-----This third amazon training.-----This third amazon training.',
     description: '',
   },
   {
     name: 'Training 4',
     excerptDesc:
-      'This is the fourth amazon training.-----This is the fourth amazon training.-----This is the fourth amazon training.-----This is the fourth amazon training.',
+      'This fourth amazon training.-----This fourth amazon training.-----This fourth amazon training.-----This fourth amazon training.',
     description: '',
   },
 ];
