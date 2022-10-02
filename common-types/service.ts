1d44aff6ce6b5f70a9eae17fd84d3fcc52c3d532
umerkang66 <umerkang66@gmail.com>
2022-10-02 10:34:37 +0500

    add description to services

--- common-types/service.ts
+++ common-types/service.ts
@@ -1,3 +1,4 @@
 export interface Service {
   name: string;
+  description: string;
 }
--- data/all-services.ts
+++ data/all-services.ts
@@ -1,13 +1,13 @@
 import { Service } from '../common-types/service';
 
 export const allServices: Service[] = [
-  { name: 'Amazon Account Management' },
-  { name: 'US LLC / UK LTD Formation' },
-  { name: 'Amazon Account Creation' },
-  { name: 'Product Hunting' },
-  { name: 'Supplier Hunting' },
-  { name: 'Product Launching' },
-  { name: 'Product Ranking' },
-  { name: 'Walmart Account Management' },
-  { name: 'eBay Account Management' },
+  { name: 'Amazon Account Management', description: '' },
+  { name: 'US LLC / UK LTD Formation', description: '' },
+  { name: 'Amazon Account Creation', description: '' },
+  { name: 'Product Hunting', description: '' },
+  { name: 'Supplier Hunting', description: '' },
+  { name: 'Product Launching', description: '' },
+  { name: 'Product Ranking', description: '' },
+  { name: 'Walmart Account Management', description: '' },
+  { name: 'eBay Account Management', description: '' },
 ];
