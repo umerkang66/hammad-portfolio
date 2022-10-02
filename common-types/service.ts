863e9207978336cd5c54c7783fc07c2aa3410b33
umerkang66 <umerkang66@gmail.com>
2022-10-02 10:35:53 +0500

    add slug to services

--- common-types/service.ts
+++ common-types/service.ts
@@ -1,4 +1,5 @@
 export interface Service {
   name: string;
   description: string;
+  slug: string;
 }
--- data/all-services.ts
+++ data/all-services.ts
@@ -1,13 +1,13 @@
 import { Service } from '../common-types/service';
 
 export const allServices: Service[] = [
-  { name: 'Amazon Account Management', description: '' },
-  { name: 'US LLC / UK LTD Formation', description: '' },
-  { name: 'Amazon Account Creation', description: '' },
-  { name: 'Product Hunting', description: '' },
-  { name: 'Supplier Hunting', description: '' },
-  { name: 'Product Launching', description: '' },
-  { name: 'Product Ranking', description: '' },
-  { name: 'Walmart Account Management', description: '' },
-  { name: 'eBay Account Management', description: '' },
+  { name: 'Amazon Account Management', description: '', slug: '' },
+  { name: 'US LLC / UK LTD Formation', description: '', slug: '' },
+  { name: 'Amazon Account Creation', description: '', slug: '' },
+  { name: 'Product Hunting', description: '', slug: '' },
+  { name: 'Supplier Hunting', description: '', slug: '' },
+  { name: 'Product Launching', description: '', slug: '' },
+  { name: 'Product Ranking', description: '', slug: '' },
+  { name: 'Walmart Account Management', description: '', slug: '' },
+  { name: 'eBay Account Management', description: '', slug: '' },
 ];
