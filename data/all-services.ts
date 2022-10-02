3517ffdbcdf3fcae499c99727eeade02662cf62c
umerkang66 <umerkang66@gmail.com>
2022-10-02 11:03:26 +0500

    fixed the bug in slug

--- data/all-services.ts
+++ data/all-services.ts
@@ -1,13 +1,62 @@
+import slugify from 'slugify';
 import { Service } from '../common-types/service';
 
+enum Names {
+  amazonAccountManagement = 'Amazon Account Management',
+  usFormation = 'US LLC / UK LTD Formation',
+  amazonAccountCreation = 'Amazon Account Creation',
+  productHunting = 'Product Hunting',
+  supplierHunting = 'Supplier Hunting',
+  productLaunching = 'Product Launching',
+  productRanking = 'Product Ranking',
+  walmartAccountManagement = 'Walmart Account Management',
+  ebayAccountManagement = 'eBay Account Management',
+}
+
 export const allServices: Service[] = [
-  { name: 'Amazon Account Management', description: '', slug: '' },
-  { name: 'US LLC / UK LTD Formation', description: '', slug: '' },
-  { name: 'Amazon Account Creation', description: '', slug: '' },
-  { name: 'Product Hunting', description: '', slug: '' },
-  { name: 'Supplier Hunting', description: '', slug: '' },
-  { name: 'Product Launching', description: '', slug: '' },
-  { name: 'Product Ranking', description: '', slug: '' },
-  { name: 'Walmart Account Management', description: '', slug: '' },
-  { name: 'eBay Account Management', description: '', slug: '' },
+  {
+    name: Names.amazonAccountManagement,
+    description: '',
+    slug: slugify(Names.amazonAccountManagement),
+  },
+  {
+    name: Names.usFormation,
+    description: '',
+    slug: slugify(Names.usFormation),
+  },
+  {
+    name: Names.amazonAccountCreation,
+    description: '',
+    slug: slugify(Names.amazonAccountCreation),
+  },
+  {
+    name: Names.productHunting,
+    description: '',
+    slug: slugify(Names.productHunting),
+  },
+  {
+    name: Names.supplierHunting,
+    description: '',
+    slug: slugify(Names.supplierHunting),
+  },
+  {
+    name: Names.productLaunching,
+    description: '',
+    slug: slugify(Names.productLaunching),
+  },
+  {
+    name: Names.productRanking,
+    description: '',
+    slug: slugify(Names.productRanking),
+  },
+  {
+    name: Names.walmartAccountManagement,
+    description: '',
+    slug: slugify(Names.walmartAccountManagement),
+  },
+  {
+    name: Names.ebayAccountManagement,
+    description: '',
+    slug: slugify(Names.ebayAccountManagement),
+  },
 ];
