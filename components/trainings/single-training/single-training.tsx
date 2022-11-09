b17523da10830cc2033fe4d19bb256cd3e9b9f6d
umerkang66 <umerkang66@gmail.com>
2022-11-09 14:39:02 +0500

    change the single training layout

--- components/trainings/single-training/single-training.tsx
+++ components/trainings/single-training/single-training.tsx
@@ -1,14 +1,31 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useEffect, useState } from 'react';
 import { Training } from '../../../common-types/training';
 import styles from './single-training.module.scss';
 
-interface SingleTrainingProps {
+interface Props {
   training: Training;
 }
 
-const SingleTraining: FC<SingleTrainingProps> = ({
-  training,
-}): ReactElement => {
+const SingleTraining: FC<Props> = ({ training }): ReactElement => {
+  const [mainDescriptions, setMainDescriptions] = useState<string[]>([]);
+  const [furtherDescriptions, setFurtherDescriptions] = useState<{
+    [key: number]: string[];
+  }>({});
+  const [whichBlockActive, setWhichBlockActive] = useState<{
+    [key: number]: boolean;
+  }>({});
+
+  useEffect(() => {
+    setMainDescriptions(training.description.map(descPoint => descPoint.name));
+
+    training.description.forEach((descPoint, i) => {
+      setFurtherDescriptions(prevFurtherDescriptions => ({
+        ...prevFurtherDescriptions,
+        [i]: descPoint.furtherDesc,
+      }));
+    });
+  }, []);
+
   return (
     <div className={styles.single_training}>
       <div className="container">
@@ -16,32 +33,49 @@ const SingleTraining: FC<SingleTrainingProps> = ({
           {training.name}
         </h2>
 
-        <h3 className="heading-secondary u-margin-bottom-medium">
+        <h3 className="heading-tertiary u-margin-bottom-medium">
           What you will learn in this course
         </h3>
 
         <div className={styles.single_training__content}>
-          <ol className={styles.single_training__list_main}>
-            {training.description.map((trainingDesc, i) => {
-              const className = trainingDesc.furtherDesc.length
-                ? 'u-margin-bottom-medium'
-                : '';
+          {mainDescriptions.map((desc, i) => {
+            return (
+              <div key={i}>
+                <div className={styles.single_training__main_desc}>
+                  <h2>{i + 1 + ': ' + desc}</h2>
 
-              return (
-                <li className={className} key={i}>
-                  {trainingDesc.name}
-                  <ol
-                    className={styles.single_training__list_secondary}
-                    type="a"
+                  {furtherDescriptions[i].length >= 1 && (
+                    <button
+                      onClick={() => {
+                        setWhichBlockActive(prevState => {
+                          return { ...prevState, [i]: !prevState[i] };
+                        });
+                      }}
+                      className={`${styles.single_training__main_desc_btn} ${
+                        whichBlockActive[i] &&
+                        styles.single_training__main_desc_btn__active
+                      }`}
+                    >
+                      <span>↓</span>
+                    </button>
+                  )}
+                </div>
+
+                {whichBlockActive[i] && (
+                  <div
+                    className={styles.single_training__further_desc_container}
                   >
-                    {trainingDesc.furtherDesc.map((furtherDescription, i) => {
-                      return <li key={i}>{furtherDescription}</li>;
-                    })}
-                  </ol>
-                </li>
-              );
-            })}
-          </ol>
+                    {furtherDescriptions[i] &&
+                      furtherDescriptions[i].map((furtherDesc, i) => (
+                        <div key={i}>
+                          <h3>{i + 1 + ': ' + furtherDesc}</h3>
+                        </div>
+                      ))}
+                  </div>
+                )}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
